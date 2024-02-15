84182f79564546a2f24b2ee875094542af71fb68
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-02-15 20:02:09 +0600

    change

--- src/components/header/Nvigation.jsx
+++ src/components/header/Nvigation.jsx
@@ -11,47 +11,27 @@ export default function Navigation() {
     <div className="container mx-auto mt-6">
       <ul className="flex flex-wrap items-center justify-center gap-5 text-xs font-semibold lg:text-base">
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             General
           </a>
         </li>
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             Business
           </a>
         </li>
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             Entertainment
           </a>
         </li>
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             Health
           </a>
         </li>
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             Science
           </a>
         </li>
@@ -61,11 +41,7 @@ export default function Navigation() {
           </a>
         </li>
         <li>
-          <a
-            href="javascript:void(0)"
-            className=" cursor-pointer"
-            onClick={handleClick}
-          >
+          <a href="#" className=" cursor-pointer" onClick={handleClick}>
             Technology
           </a>
         </li>
